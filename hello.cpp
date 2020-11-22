5bdbd3b4a8ff4b02048ac7e43dd6409795ac2456
mtho <michael_t_ho@hotmail.com>
2020-11-22 09:06:48 -0800

    add fun

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,21 @@
 #include<iostream>
+int f(int x)
+{
+	if (x<5)
+	{
+		std::cout << "less than 5 \n";
+	}
+	else
+	{
+		std::cout << "not less than 5 \n";
+	}
+	return 0;
+}
+
 int main()
 {
 std::cout << "hello world , dude \n";
 std::cout << "update 1 \n";
-
+f(3);
 return 0;
 }
